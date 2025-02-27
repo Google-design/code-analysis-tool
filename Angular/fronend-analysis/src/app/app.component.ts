d0f21f8079f7b139f8100cba88dcb46f1a3bae53
Fnu Abdullah <111256456+Google-design@users.noreply.github.com>
2025-02-27 12:26:37 -0600

    Use the public site for post requests

--- Angular/fronend-analysis/src/app/app.component.ts
+++ Angular/fronend-analysis/src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent {
 
     const requestBody = { repo_url: this.repoUrl };  // Ensure repo_url is correctly passed
 
-    this.http.post<AnalysisResponse>('http://127.0.0.1:5000/run-analyzer', requestBody)
+    this.http.post<AnalysisResponse>('https://codeanalysis-42512.web.app/run-analyzer', requestBody)
       .pipe(
         catchError(error => {
           console.error('Error:', error);
@@ -68,7 +68,7 @@ export class AppComponent {
     const formData = new FormData();
     formData.append('file', this.selectedFile);
 
-    this.http.post<AnalysisResponse>('http://127.0.0.1:5000/run-analyzer', formData)
+    this.http.post<AnalysisResponse>('https://codeanalysis-42512.web.app/run-analyzer', formData)
       .pipe(
         catchError(error => {
           console.error('Error:', error);
